f8d9699824f60ad896e0cbf13475f505d47962bf
Daniel Carter <dtcarter84@gmail.com>
2018-07-25 17:22:04 -0400

    Add files via upload

--- src/routes/speakeasy-practice.js
+++ src/routes/speakeasy-practice.js
@@ -11,7 +11,7 @@ var jsonParser = require("body-parser").json;
 //Speakeasy main page.
 
 router.get("/", function(req, res, next) {
-  mongoose.model("Drinks").find({}, function (err, Drinks) {
+  mongoose.model("Drinks").find({deleted: {$ne: true}}, function (err, Drinks) {
     if (err) {
         console.log(err);
         return res.status(500).json(err);
@@ -23,14 +23,28 @@ router.get("/", function(req, res, next) {
 //Post new drink information
 router.post("/", function(req, res, next) {
   const Drink = mongoose.model('Drinks');
-  const drinkData = {
+  /* const drinkData = {
     name: req.body.name,
     description: req.body.description,
     rate: req.body.rate,
     comments: req.body.comments
-  };
+  }; */
 
   Drink.create(drinkData, function(err, newDrink) {
+        /* if(drinkData.rate < 1) {
+            const err = new Error("Please choose a number between 1 and 5.");
+            return err;
+        } else if (drinkData.rate > 5) {
+            const err = new Error("Number is too high. Please choose a number between 1 and 5.");
+            return err;
+        } else if (err) {
+            console.error(err);
+            return res.status(500).json(err);
+        }
+
+        res.json(newDrink);
+  }); */
+      
     if (err) {
       console.error(err);
       return res.status(500).json(err);
@@ -41,6 +55,7 @@ router.post("/", function(req, res, next) {
 });
 
 //Route GETS specific drink 
+//an error seems to occur in my route on line 62, there seems to be a problem with my Drinks.find() method
 router.get("/:drinkID", function(req, res, next) {
   const Drinks = mongoose.model('Drinks');
   const drinkID = req.params.drinkID;
@@ -51,9 +66,26 @@ router.get("/:drinkID", function(req, res, next) {
   }
 
   res.json(drinkID);
+
 });
 
 //Edits a particular drink
+router.put('/:drinkID', function(req, res, next) {
+  const Drinks = mongoose.model('Drinks');
+  const drinkID = req.params.drinkID;
+  console.log(req.params.drinkID);
+  
+  Drinks.findById(drinkID, function(err, drink) {
+    if (err) {
+      console.error(err);
+      return res.status(500).json(err);
+    }
+    if (!drink) {
+      return res.status(404).json({message: "Drink not found"});
+    }
+  });
+
+/*
 router.put('/:drinkID', function(req, res, next) {
   const Drinks = mongoose.model('Drinks');
   const drinkID = req.params.drinkID;
@@ -81,7 +113,7 @@ router.put('/:drinkID', function(req, res, next) {
     })
   
   })
-});
+}); */
 
 /* router.put("/:drinkID", function (req, res) {
     Drinks.findByIdAndUpdate(req.params.drinkID, req.body, {new: true}, function (err, drink) {
